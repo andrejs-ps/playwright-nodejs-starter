8c85b7d712fc406e404666e1f2a1adf725a31dc3
Andrejs <56889270+andrejs-ps@users.noreply.github.com>
2024-01-12 16:06:21 +0100

    Update playwright.config.ts
    Update the config file to match the video demo

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,9 +2,5 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
-  fullyParallel: true,
-  reporter: 'html',
-  expect: {
-    timeout: 3000
-  }
+  reporter: 'html'
 });
